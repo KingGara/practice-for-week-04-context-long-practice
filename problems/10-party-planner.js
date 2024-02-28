14e84454b9da73fcbdf7496bfe7358f0380bf587
KingGara <evenoddvergara@gmail.com>
2024-02-28 13:03:03 -0800

    final Commit

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,6 +1,31 @@
-// Your code here
+class PartyPlanner {
+	constructor() {
+		this.guestList = [];
+	}
+	
+	addToGuestList(name) {
+		this.guestList.push(name);
+	}
 
+	throwParty() {
+		if (this.guestList.length === 0) {
+			return "Gotta add people to the guest list"
+		} else {
+			const guests = this.guestList.join(' and ');
+			return `Welcome to the party ${guests}`
+		}
+	}
+}
 
+const party = new PartyPlanner();
+
+console.log(party.throwParty());
+
+party.addToGuestList("James");
+console.log(party.throwParty());
+
+party.addToGuestList("Alvin");
+console.log(party.throwParty());
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
